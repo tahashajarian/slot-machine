b88bd27e35120065079353fd77eb070490d14022
taha <shajariantaha@gmial.com>
2021-04-18 18:33:09 +0430

    refactor code and add some comments

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// this function just create game class and property that needed
+const startGame = () => {
+  const gameData = {};
+  gameData.items = items;
+  gameData.reelCount = 3;
+  gameData.spinTime = 3500;
+  gameData.startScore = 100;
+  gameData.mode = items.modes.random;
+  gameData.dataPayTable = dataPayTable;
+  gameData.redLine = document.getElementById("red-line");
+  gameData.payTable = document.getElementById("payTable");
+  gameData.modal = document.getElementById("modal-container");
+  gameData.closeModal = document.getElementById("closeModal");
+  gameData.scoreContainer = document.getElementById("credit");
+  gameData.spinButton = document.getElementById("spin-button");
+  gameData.lineSelect = document.getElementById("select-line");
+  gameData.switchButton = document.getElementById("switch-button");
+  gameData.background = document.getElementById("background-blur");
+  gameData.symbolSelect = document.getElementById("select-symbole");
+  gameData.reelContainer = document.getElementById("reelContainer");
+  gameData.spinFixedButton = document.getElementById("fixed-button");
+  gameData.reelItmes = [
+    items.barX3.name,
+    items.bar.name,
+    items.barX2.name,
+    items.se7en.name,
+    items.cherry.name,
+    items.barX3.name,
+    items.bar.name,
+    items.barX2.name,
+  ];
+  const gameManager = new GameManager({ ...gameData });
+  gameManager.preparePlayGround();
+};
+
+// this class is main part of game and controll every thing
 class GameManager {
   constructor({
     payTable,
@@ -45,7 +81,7 @@ class GameManager {
   preparePlayGround() {
     createPayTable(this.payTable, this.dataPayTable);
     createReels(this.reelCount, this.reelContainer, this.reelItmes);
-    createSelctes(this.items, this.symbolSelect, this.lineSelect);
+    createSelectes(this.items, this.symbolSelect, this.lineSelect);
     this.spinButton.onclick = this.spin;
     this.switchButton.onchange = this.switchMode;
     this.spinFixedButton.onclick = this.fixedSpin;
@@ -77,12 +113,13 @@ class GameManager {
     }
   };
 
+  // in fixed mode spin button called this method
   fixedSpin = () => {
     const selectedSymbol = this.symbolSelect.value;
     const selectedLine = this.lineSelect.value;
     if (selectedSymbol && selectedLine) {
       this.toggleModal(false);
-      const index = getIndexOfObject(this.items.availableItems, selectedSymbol);
+      const index = getIndexOfArray(this.items.availableItems, selectedSymbol);
       const fixedResult = [
         {
           symbol: this.items.availableItems[index],
@@ -110,6 +147,7 @@ class GameManager {
     }
   };
 
+  // to hide and show modal (select line and symbol)
   toggleModal(status) {
     if (status) {
       this.modal.style.visibility = "visible";
@@ -118,6 +156,7 @@ class GameManager {
     }
   }
 
+  // to active and deactive spin button
   toggleSpining(status) {
     if (status) {
       this.spinButton.classList.remove("active");
@@ -127,6 +166,7 @@ class GameManager {
     this.spining = status;
   }
 
+  // this method get the result first and start animate reels and stop on the result
   spinAnimation(result) {
     this.toggleSpining(true);
     if (!this.reels) {
@@ -150,23 +190,22 @@ class GameManager {
     this.updateScore(-1);
   }
 
+  // this method return a random result for 3 reel (used in random mode)
   spinResultRandom() {
-    return [
-      {
+    const randomResult = [];
+    for (let i = 0; i < this.reelCount; i++) {
+      randomResult[i] = {
         symbol: randomItemFromAraay(this.items.availableItems),
         line: randomItemFromAraay(this.items.availablePosition),
-      },
-      {
-        symbol: randomItemFromAraay(this.items.availableItems),
-        line: randomItemFromAraay(this.items.availablePosition),
-      },
-      {
-        symbol: randomItemFromAraay(this.items.availableItems),
-        line: randomItemFromAraay(this.items.availablePosition),
-      },
-    ];
+      };
+    }
+    return randomResult;
   }
 
+  // this method get the 3 lines shown sybmol
+  // and stat check matching from top of pay-table
+  // and return match score
+  // and also return index of tabel row and line of reels for blinking
   calculateScore(lines) {
     for (let i = 0; i < dataPayTable.length; i++) {
       let activeLines = [];
@@ -212,6 +251,7 @@ class GameManager {
     return 0;
   }
 
+  // on dbclink on score this mthod called and in fixed mode change score to editable mode
   changeScoreByHand = () => {
     if (this.mode === this.items.modes.random) {
       return;
@@ -222,6 +262,7 @@ class GameManager {
     }
   };
 
+  // when score changed to editable mode on blur this method called
   onBlurScoreContainer = () => {
     console.log(this.scoreContainer.innerText);
     this.scoreContainer.removeAttribute("contentEditable");
@@ -235,6 +276,8 @@ class GameManager {
     }
   };
 
+  // this mtheod get the row table and the line of reels and blink the table row and
+  // and red line on the line
   blinkTableAndSlot(tableRow, reelRow) {
     const theRow = this.payTable.getElementsByClassName("row-pay-table")[
       tableRow
@@ -243,7 +286,8 @@ class GameManager {
     this.redLine.className = this.items.availablePosition[reelRow];
   }
 
-  removeBlinking = (x) => {
+  // this mthdo remove red line and blinked row of table
+  removeBlinking = () => {
     this.redLine.className = "";
     const allRows = this.payTable.getElementsByClassName("row-pay-table");
     for (let i = 0; i < allRows.length; i++) {
@@ -252,6 +296,8 @@ class GameManager {
     }
   };
 
+  // this method get the score and update score by add property to curren score
+  // if force property was true the score will equil of score propery (not add)
   updateScore(score, force) {
     if (force) {
       this.score = score;
@@ -261,6 +307,8 @@ class GameManager {
     animateValue(this.scoreContainer, this.score, 1500);
   }
 
+  // for calculate score we need the all three line and this mehod get the reslut
+  // and return three line shown in slot machine
   get3Lines(result) {
     const lines = [[], [], []];
     const calcIndex = (index) => {
@@ -294,39 +342,227 @@ class GameManager {
   }
 }
 
-// this function just create game class and property that needed
-const startGame = () => {
-  const gameData = {};
-  gameData.items = items;
-  gameData.mode = items.modes.random;
-  gameData.spinTime = 3500;
-  gameData.startScore = 100;
-  gameData.dataPayTable = dataPayTable;
-  gameData.payTable = document.getElementById("payTable");
-  gameData.reelContainer = document.getElementById("reelContainer");
-  gameData.redLine = document.getElementById("red-line");
-  gameData.scoreContainer = document.getElementById("credit");
-  gameData.spinButton = document.getElementById("spin-button");
-  gameData.switchButton = document.getElementById("switch-button");
-  gameData.background = document.getElementById("background-blur");
-  gameData.modal = document.getElementById("modal-container");
-  gameData.spinFixedButton = document.getElementById("fixed-button");
-  gameData.symbolSelect = document.getElementById("select-symbole");
-  gameData.lineSelect = document.getElementById("select-line");
-  gameData.closeModal = document.getElementById("closeModal");
-  gameData.reelCount = 3;
-  gameData.reelItmes = [
-    items.barX3.name,
-    items.bar.name,
-    items.barX2.name,
-    items.se7en.name,
-    items.cherry.name,
-    items.barX3.name,
-    items.bar.name,
-    items.barX2.name,
-  ];
-  const gameManagert = new GameManager({ ...gameData });
-  gameManagert.preparePlayGround();
+// this is all items we need in this game
+const items = {
+  cherry: {
+    name: "cherry",
+  },
+  se7en: {
+    name: "se7en",
+  },
+  cherrySe7en: {
+    name: "cherry-se7en",
+  },
+  bar: {
+    name: "bar",
+  },
+  barX2: {
+    name: "barX2",
+  },
+  barX3: {
+    name: "barX3",
+  },
+  bars: {
+    name: "bars",
+  },
+  position: {
+    any: "any",
+    top: "top",
+    center: "center",
+    bottom: "bottom",
+  },
+  availablePosition: ["top", "center", "bottom"],
+  availableItems: [
+    {
+      name: "cherry",
+      center: -270,
+      top: -360,
+      bottom: -180,
+    },
+    {
+      name: "se7en",
+      center: -180,
+      top: -270,
+      bottom: -90,
+    },
+    {
+      name: "bar",
+      center: 0,
+      top: -90,
+      bottom: -360,
+    },
+    {
+      name: "barX2",
+      center: -90,
+      top: -180,
+      bottom: 0,
+    },
+    {
+      name: "barX3",
+      center: -360,
+      top: 0,
+      bottom: -270,
+    },
+  ],
+  modes: {
+    random: "random",
+    fixed: "fixed",
+  },
+};
+
+// this is data used in pay-table
+const dataPayTable = [
+  {
+    items: [items.cherry.name, items.cherry.name, items.cherry.name],
+    position: items.position.bottom,
+    score: 4000,
+  },
+  {
+    items: [items.cherry.name, items.cherry.name, items.cherry.name],
+    position: items.position.top,
+    score: 2000,
+  },
+  {
+    items: [items.cherry.name, items.cherry.name, items.cherry.name],
+    position: items.position.center,
+    score: 1000,
+  },
+  {
+    items: [items.se7en.name, items.se7en.name, items.se7en.name],
+    position: items.position.any,
+    score: 150,
+  },
+  {
+    items: [
+      items.cherrySe7en.name,
+      items.cherrySe7en.name,
+      items.cherrySe7en.name,
+    ],
+    position: items.position.any,
+    score: 75,
+    combines: ["cherry", "se7en"],
+  },
+  {
+    items: [items.barX3.name, items.barX3.name, items.barX3.name],
+    position: items.position.any,
+    score: 50,
+  },
+  {
+    items: [items.barX2.name, items.barX2.name, items.barX2.name],
+    position: items.position.any,
+    score: 20,
+  },
+  {
+    items: [items.bar.name, items.bar.name, items.bar.name],
+    position: items.position.any,
+    score: 10,
+  },
+  {
+    items: [items.bars.name, items.bars.name, items.bars.name],
+    position: items.position.any,
+    score: 5,
+    combines: ["bar", "barX2", "barX3"],
+  },
+];
+
+// this funtion get an array and return a random item in that array
+var randomItemFromAraay = (items) => {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+// this funtion get an array of option and item and return a index of that object contain the item
+const getIndexOfArray = (array, name) => {
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (element.name === name) return i;
+  }
+  return 0;
+};
+
+// this function is effect to update socre and get
+// the object is elemnt that gonna change and the end that is final number and the time for duration update the number
+const animateValue = (obj, end, duration) => {
+  let startTimestamp = null;
+  const start = Number(obj.innerText);
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start);
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+};
+
+// this function create pay-table
+const createPayTable = (table, dataTable) => {
+  dataTable.forEach((row) => {
+    const divRow = document.createElement("div");
+    divRow.classList.add("row-pay-table");
+    const divScore = document.createElement("div");
+    divScore.innerHTML = row.score;
+    divScore.classList.add("row-pay-table-score");
+    const divPosition = document.createElement("div");
+    divPosition.innerHTML = row.position;
+    divPosition.classList.add("row-pay-table-position");
+    const divItems = document.createElement("div");
+    divItems.classList.add("row-pay-table-items");
+    row.items.forEach((item) => {
+      const spanItem = document.createElement("span");
+      spanItem.classList.add("card");
+      spanItem.classList.add(item);
+      divItems.appendChild(spanItem);
+    });
+    divRow.appendChild(divItems);
+    divRow.appendChild(divPosition);
+    divRow.appendChild(divScore);
+    table.appendChild(divRow);
+  });
+};
+
+// this functon crete reels in solot machine
+const createReels = (reelCount, reelContainer, reelItmes) => {
+  for (let i = 0; i < reelCount; i++) {
+    const reel = document.createElement("div");
+    reel.classList.add("reel");
+    const reelScrollAble = document.createElement("div");
+    reelScrollAble.className = "reel-scroll-able";
+    reelItmes.forEach((item) => {
+      const card = document.createElement("span");
+      card.classList.add("card");
+      card.classList.add(item);
+      reelScrollAble.appendChild(card);
+    });
+    reel.appendChild(reelScrollAble);
+    const reelShadow = document.createElement("div");
+    reelShadow.classList.add("reel-shadow");
+    reelShadow.innerHTML = "<span class='reel-shadow-light'></span>";
+    const reelShadowContainer = document.createElement("div");
+    reelShadowContainer.classList.add("reel-shadow-container");
+    reelShadowContainer.appendChild(reel);
+    reelShadowContainer.appendChild(reelShadow);
+    reelContainer.appendChild(reelShadowContainer);
+  }
+};
+
+// this functon create select opetion in modal (in fixed mode to select symbol and line)
+const createSelectes = (items, symbolSelect, lineSelect) => {
+  const symbols = items.availableItems;
+  const lines = items.availablePosition;
+
+  symbols.forEach((sym) => {
+    const option = document.createElement("option");
+    option.innerHTML = sym.name;
+    option.value = sym.name;
+    symbolSelect.appendChild(option);
+  });
+  lines.forEach((line) => {
+    const option = document.createElement("option");
+    option.innerHTML = line;
+    option.value = line;
+    lineSelect.appendChild(option);
+  });
 };
 
 // start game call after all loaded
